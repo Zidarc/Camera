ac67476e7981f8090165ff5757d208fb87b2f01b
alihussain <ali.hussaino71205o@gmail.com>
2024-01-03 19:37:06 +0500

    added the lock image made the button work

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 async function check() {
     const input = document.getElementById("textInput").value;
-        if (input.trim() === "I am foolish") {
-            document.getElementById("information").innerText = "success";
-            window.location.href = '/success.html';
-        } else {
-            informationBox.textContent = "Failure!";
-            document.getElementById("information").innerText = "failure";
-        }
+    const informationBox = document.querySelector(".information");
+
+    if (input.trim() === "I am foolish") {
+        informationBox.textContent = "Success!";
+        window.location.href = '/success.html';
+    } else {
+        informationBox.textContent = "Failure!";
+    }
 }
