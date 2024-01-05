6de7a37e7c712d0f119c0cfe44d540006fb6c7ac
Ali Hussain <121773452+Zidarc@users.noreply.github.com>
2024-01-05 16:13:07 +0500

    Update index.js
    Changed the password

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ async function check() {
     const input = document.getElementById("textInput").value;
     const informationBox = document.querySelector(".information");
 
-    if (input.trim() === "I am foolish") {
+    if (input.trim() === "8888") {
         informationBox.textContent = "Success!";
         window.location.href = '/success.html';
     } else {
